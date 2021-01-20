22c3e4de2ca2976a970cd847a02271579115f390
sujing.su <sujing.su@bytedance.com>
2021-01-20 14:54:52 +0800

    fix: 修正"退出登录"错别字

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -4,5 +4,5 @@ export default {
   'menu.Account': '个人页',
   'menu.Account.Center': '用户中心',
   'menu.Account.Settings': '个人设置',
-  'menu.Account.Logout': '退出登陆',
+  'menu.Account.Logout': '退出登录',
 };
